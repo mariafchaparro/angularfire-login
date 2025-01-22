d06c6f0d80bbe60dc529a55466597286c9aa6c8f
Fer <maferch2002@gmail.com>
2025-01-21 23:05:04 -0400

    Fix browser animation error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideToastr({
