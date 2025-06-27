f3c7ad543996647d5496fcae3782e5583856e54f
Maria Chaparro <maferch2002@gmail.com>
2025-06-26 22:32:44 -0400

    fix guard and dashboard details

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,27 +1,38 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { getIdTokenResult, User } from 'firebase/auth';
 import { ToastrService } from 'ngx-toastr';
 
 export const roleGuard: CanActivateFn = async (route, state) => {
-  const auth = inject(Auth)
+  const auth = inject(Auth);
   const router = inject(Router);
   const toastr = inject(ToastrService);
 
-  const user = auth.currentUser
+  // Wait for firebase to load the user
+  const user = await new Promise<User | null>((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+
   if (!user) {
-    router.navigate(['/login'])
+    router.navigate(['/login']);
+    return false;
   }
 
-  const tokenResult = await user?.getIdTokenResult()
-  const role = tokenResult?.claims['role']
+  // Force the user to refresh the token to get the latest claims
+  await user.getIdToken(true);
+  const tokenResult = await getIdTokenResult(user);
+  const role = (tokenResult.claims as any).role;
 
   if (role === 'admin') {
-    return true
+    return true;
   }
 
-  toastr.error('No tienes permiso para acceder a esta página.', 'Acceso Denegado');
+  toastr.error('No tienes permiso para acceder');
   router.navigate(['/home']);
   return false;
 };
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  auth = inject(Auth)
+  router = inject(Router)
+  
+  onClick() {
+    signOut(this.auth)
+      .then(() => this.router.navigate(['/login']))
+      .catch((err) => console.log(err))
+  }
 }
